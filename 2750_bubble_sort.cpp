93b10da63180c1eb069a4bc764491b1f45f615ce
SeunghwanByun <49049277+SeunghwanByun@users.noreply.github.com>
2021-01-17 04:01:52 +0900

    Update 2750_bubble_sort.cpp

--- 2750_bubble_sort.cpp
+++ 2750_bubble_sort.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
-void InsertionSort(int *arr, int length) {
-	int key;
-
-	for (int i = 1; i < length; i++) {
-		if (arr[i - 1] <= arr[i])
-			continue;
-
-		if (arr[i - 1] > arr[i]) {
-			key = arr[i];
-			for (int j = 0; j < i; j++) {
-				if (arr[j] > key) {
-					memmove(&arr[j + 1], &arr[j], sizeof(int) * (i - j));
-					arr[j] = key;
-					break;
-				}
+void BubbleSort(int *arr, int length) {
+	for (int l = length - 1; l > 0; l--) {
+		for (int i = 0; i < l; i++) {
+			if (arr[i] >= arr[i + 1]) {
+				int temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
 			}
 		}
-	}
+	}	
 }
 
 int main() {
 	int N;
+
 	cin >> N;
 
 	int *arr = new int[N];
@@ -32,10 +24,8 @@ int main() {
 	for (int i = 0; i < N; i++)
 		cin >> arr[i];
 
-	InsertionSort(arr, N);
+	BubbleSort(arr, N);
 
 	for (int i = 0; i < N; i++)
 		cout << arr[i] << endl;
-
-	return 0;
 }
