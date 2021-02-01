47c9fc82119eb93b2c3c3d866653f953663ac3ab
SeunghwanByun <rapping44@naver.com>
2021-02-01 18:47:38 +0900

    Upload code.

---
+++ 2869.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B, V;
+
+    cin >> A >> B >> V;
+
+    if((V - B) % (A - B) == 0)
+        cout << (V - B) / (A - B);
+    else
+    {
+        cout << (V - B) / (A - B) + 1;
+    }
+    
+    return 0;
+}
