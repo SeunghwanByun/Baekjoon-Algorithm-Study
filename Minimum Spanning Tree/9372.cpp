4b1f18228a59bc12173642c2adce149e1c3dbda5
Seunghwan Byun <49049277+SeunghwanByun@users.noreply.github.com>
2021-05-05 19:28:11 +0900

    Create 9372.cpp

---
+++ Minimum Spanning Tree/9372.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+
+int main() {
+	int T;
+	scanf("%d", &T);
+
+	for (int t = 0; t < T; ++t) {
+		int N, M;
+		scanf("%d %d", &N, &M);
+
+		for (int m = 0; m < M; ++m) {
+			int tempA, tempB;
+			scanf("%d %d", &tempA, &tempB);
+		}
+		printf("%d\n", N - 1);
+	}
+
+	return 0;
+}
